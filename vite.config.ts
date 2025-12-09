09168088ca06cd37cd965abe5f1611859b1d885f
Rohan <rohann.m93@gmail.com>
2025-12-09 16:36:09 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     define: {
       // Polyfill process.env.API_KEY so it works in the browser
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
     },
   };
 });
